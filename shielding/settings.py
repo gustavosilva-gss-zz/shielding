09f920438506bcf7e24d8373e85d921860177d75
Gustavo Silva <gustavosilva@pop-os.localdomain>
2020-09-05 11:59:54 -0300

    debug

--- shielding/settings.py
+++ shielding/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '27%2!rm&y0046cg*lonrqho%khb!(1%nh-+dj6xl--4y!5k&!s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 

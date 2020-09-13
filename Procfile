5d34d5c164d7931955d5ecab71cab5e25c85f45d
Gustavo Silva <gustavosilva@pop-os.localdomain>
2020-09-13 20:11:08 -0300

    fix status on donation model

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-docker run -p 6379:6379 -d redis:5
 web: gunicorn shielding.wsgi --log-file -
--- website/models.py
+++ website/models.py
@@ -43,20 +43,27 @@ class Volunteer(models.Model):
         }
 
 class Donation(models.Model):
-    STATUS = (
-        ('S', 'submitted'),
-        ('D', 'delivered'),
-        ('C', 'canceled'),
-    )
-
     volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE, related_name="donations")
     establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE, related_name="donations")
     quantity = models.PositiveIntegerField()
     volunteer_delivering = models.BooleanField()
-    status = models.CharField(max_length=1, null=False, choices=STATUS, default='S')
     submission_time = models.DateTimeField(auto_now_add=True)
     estimated_time = models.DateTimeField()
     delivered_time = models.DateTimeField(null=True)
+
+    #at first I used a choices field for the status but heroku wouldnt work with it
+    #more details here: https://stackoverflow.com/questions/63755702/
+
+    '''
+    the options for status are:
+        submitted,
+        delivered,
+        canceled
+    '''
+
+    #all up to 10 charactes, thats why thats the limit
+
+    status = models.CharField(max_length=10, null=False, default='submitted')
 
     def serialize(self):
         return {
@@ -65,7 +72,7 @@ class Donation(models.Model):
             "establishment": self.establishment.id,
             "quantity": self.quantity,
             "volunteer_delivering": self.volunteer_delivering,
-            "status": self.get_status_display(),
+            "status": self.status,
             "submission_time": self.submission_time.strftime("%b %-d %Y, %-I:%M %p"),
             "estimated_time": self.estimated_time.strftime("%b %-d %Y, %-I:%M %p"),
             "delivered_time": self.delivered_time.strftime("%b %-d %Y, %-I:%M %p") if self.delivered_time else None,

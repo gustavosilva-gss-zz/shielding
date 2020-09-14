49054edb8fca22573a8629b9f8d9159a2b443b93
Gustavo Silva <gustavosilva@pop-os.localdomain>
2020-09-14 18:11:52 -0300

    asgi

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,5 +1,3 @@
-import os
-import channels.asgi
+from channels.layers import get_channel_layer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
-channel_layer = channels.asgi.get_channel_layer()
+channel_layer = get_channel_layer()
